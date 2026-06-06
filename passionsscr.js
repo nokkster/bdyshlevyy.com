d02790a6037a541a8bc92cec4a82f05782f97125
bdyshlev <bohdan.dyshlevyy@inria.fr>
2026-06-06 19:27:04 +0200

    Modifications

--- passionsscr.js
+++ passionsscr.js
@@ -1,43 +1,40 @@
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 0) {
+    document.querySelector("header").classList.add("scrolled");
+  } else {
+    document.querySelector("header").classList.remove("scrolled");
+  }
+});
+
 document.addEventListener("DOMContentLoaded", function () {
   function togglePopup(popupId) {
     const popup = document.getElementById(popupId);
-    const video = popup.querySelector("video"); // Get the video element inside the popup
-    const iframe = popup.querySelector("iframe"); // Get the iframe element inside the popup
+    const video = popup.querySelector("video");
+    const iframe = popup.querySelector("iframe");
 
     popup.classList.toggle("active");
 
-    // Pause the video when the popup is closed
     if (!popup.classList.contains("active") && video) {
       video.pause();
     }
 
-    // Pause the iframe video when the popup is closed
     if (!popup.classList.contains("active") && iframe) {
       const iframeSrc = iframe.src;
-      iframe.src = iframeSrc; // Reloads the iframe, stopping the video
+      iframe.src = iframeSrc;
     }
   }
-  
-  const showPopupBtn1 = document.getElementById("show-popup-btn-1");
-  showPopupBtn1.addEventListener("click", function() {
-    togglePopup("popup-1");
+
+  document.querySelectorAll("button[id^='show-popup-btn-']").forEach(function (button) {
+    button.addEventListener("click", function () {
+      const number = button.id.replace("show-popup-btn-", "");
+      togglePopup("popup-" + number);
+    });
   });
 
-  const showPopupBtn2 = document.getElementById("show-popup-btn-2");
-  showPopupBtn2.addEventListener("click", function() {
-    togglePopup("popup-2");
-  });
-
-  const showPopupBtn3 = document.getElementById("show-popup-btn-3");
-  showPopupBtn3.addEventListener("click", function() {
-    togglePopup("popup-3");
-  });
-
-  const closeBtns = document.querySelectorAll(".popup .close-btn");
-  closeBtns.forEach(function(btn) {
+  document.querySelectorAll(".popup .close-btn").forEach(function (btn) {
     btn.addEventListener("click", function (event) {
       event.stopPropagation();
-      const popupId = btn.closest('.popup').id;
+      const popupId = btn.closest(".popup").id;
       togglePopup(popupId);
     });
   });
